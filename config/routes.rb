9414b9c3e6d813553698470daf6eed4392138fec
Ivan Vergés <ivan@pokecode.net>
2024-07-02 17:35:20 +0200

    add iframe plausible

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 require "sidekiq/web"
+require "sidekiq/cron/web"
 
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
-  mount Decidim::Core::Engine => "/"
   authenticate :user, ->(u) { u.admin? } do
     mount Sidekiq::Web => "/sidekiq"
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :iframe, only: [:index]
+  end
+
+  mount Decidim::Core::Engine => "/"
 end
